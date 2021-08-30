95790e5cba1ac3e7ae349c6dd0dd68254c477e67
crossihuy <crossihuy@ivytech.edu>
2021-08-30 07:51:44 -0400

    idk

---
+++ M01_Code_Smell (1).py
@@ -0,0 +1,7 @@
+print("What is my Purpose?")
+print("You display simple text to a screen for students to fix")
+print("Oh my god.")
+print("this is a pain to push to git")
+
+
+
---
+++ M02_Code_Smell.py
@@ -0,0 +1,13 @@
+food_stuff = float(input("How much was your meal? "))
+uncle_sam = 1.08
+pay_the_server = 1.15
+corporate_cut = uncle_sam * food_stuff
+pocket_book_pain = str(corporate_cut + pay_the_server)
+string_value = str(pocket_book_pain)
+print("You need to leave " + string_value + " on the table to cover tax and tip.")
+
+
+
+
+
+
