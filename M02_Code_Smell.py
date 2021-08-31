a876eb2f7fb982526a86b54f64c0603ea19165d2
crossihuy <crossihuy@ivytech.edu>
2021-08-31 11:32:16 -0400

    added round

--- M02_Code_Smell.py
+++ M02_Code_Smell.py
@@ -2,8 +2,9 @@ food_stuff = float(input("How much was your meal? "))
 uncle_sam = 1.08
 pay_the_server = 1.15
 corporate_cut = uncle_sam * food_stuff
-pocket_book_pain = str(corporate_cut + pay_the_server)
-string_value = str(pocket_book_pain)
+pocket_book_pain = corporate_cut + pay_the_server
+rounded = round(pocket_book_pain, 2)
+string_value = str(rounded)
 print("You need to leave " + string_value + " on the table to cover tax and tip.")
 
 
