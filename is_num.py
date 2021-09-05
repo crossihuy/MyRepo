41a95caf0fadac82f35a6f4c58fa42657bfafe01
crossihuy <crossihuy@ivytech.edu>
2021-09-04 23:23:04 -0400

    created new

---
+++ is_num.py
@@ -0,0 +1,11 @@
+def is_num(question):
+    while True:
+        try:
+            num = int(input(question))
+            break
+        except ValueError:
+            print("You did not give a number.")
+            continue
+    return num
+
+
---
+++ use_is_num.py
@@ -0,0 +1,5 @@
+from is_num import is_num
+x = is_num("Give me a number: ")
+print(x)
+y = is_num("How many friends do you have ")
+print(y)
