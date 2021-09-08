a7c6e4722290e28cfde19936a3a41fc796c63097
crossihuy <crossihuy@ivytech.edu>
2021-09-07 22:55:52 -0400

    updated game will not go past random num

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,16 +1,21 @@
-max_tries = 5
-while   max_tries > 0:
-    num1 = int(input("pick a number from 1-100. "))
-    if num1 > 100:
-        print("not a number from 1-100, guess again")
-    if num1 < 0:
-        print("not a number from 1-100, guess again")
-    if num1 == 17:
-        print("You Win")
-    elif num1 < 17:
-        print("Sorry, your guess is to low try again")
-    elif num1 >17, ==100:
-        print("Sorry, your guess is to high try again")
-max_tries -= 1
-raise ValueError("you loose")
+import random
+n = random.randint(1, 100)
+print(n)
 
+guess_count = 0
+guess_limit = 5
+while guess_count <= guess_limit :
+    guess_count =+ 1
+while True:
+
+    if n > 100:
+        print("not a number from 1-100, guess again")
+    elif n < 0:
+        print("not a number from 1-100, guess again")
+    elif n == 80:
+        print("You Win")
+    elif n < 80:
+        print("Sorry, your guess is to low try again")
+    elif n > 80:
+        print("Sorry, your guess is to high try again")
+        raise ValueError("you loose")
