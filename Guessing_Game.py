f86a538bf2880c2d18347a3d8e096dc3666aecf9
crossihuy <crossihuy@ivytech.edu>
2021-09-08 09:27:37 -0400

    with help

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,21 +1,26 @@
 import random
-n = random.randint(1, 100)
-print(n)
-
-guess_count = 0
-guess_limit = 5
-while guess_count <= guess_limit :
-    guess_count =+ 1
 while True:
-
+    n = random.randint(1, 100)
     if n > 100:
         print("not a number from 1-100, guess again")
     elif n < 0:
         print("not a number from 1-100, guess again")
-    elif n == 80:
-        print("You Win")
-    elif n < 80:
-        print("Sorry, your guess is to low try again")
-    elif n > 80:
-        print("Sorry, your guess is to high try again")
-        raise ValueError("you loose")
+    else:
+        break
+
+print(n)
+count = 0
+while count < 5:
+    while True:
+        try:
+            num1 = int(input("Guess a number "))
+            break
+        except ValueError:
+            print("Please guess a number. ")
+            continue
+    if num1 < n  :
+        print("your guess is to low")
+    elif num1 > n :
+        print("your guess is to high. ")
+
+    count += 1
