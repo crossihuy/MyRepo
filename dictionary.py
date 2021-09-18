fda0a25d45f6ad1266fa9baa5441bd4efa7aa276
crossihuy <crossihuy@ivytech.edu>
2021-09-18 17:42:24 -0400

    new code

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,4 @@
-letter_grades = {"A":4, "B":3, "C":2, "D":1, "F":0}   # A is the key place, 4.0 is the value place
+letter_grades = {"A":4,"B":3,"C":2,"D":1,"F":0}   # A is the key place, 4.0 is the value place
 print(letter_grades)
 letter_grades.update({"A":4.0, "B":3.0, "C":2.0, "D":1.0, "F":0.0})#to up date all,
 
@@ -24,8 +24,8 @@ x = get_key(2.0, letter_grades)
 print(x)
 
 #how to count from a list use a dictionary
- grades = ["A", "B", "A", "F", "D", "A", "F", "C", "A", "A", "A", "B", "A", "B"]
- count = {}
- for i in grades:
-     count[i] = count.get(i, 0) + 1  #counts from 0 if none there
- print(count)  # not working with other code above this code starting on 27-31
+grades = ["A", "B", "A", "F", "D", "A", "F", "C", "A", "A", "A", "B", "A", "B"]
+count = {}
+for i in grades:
+    count[i] = count.get(i, 0) + 1  #counts from 0 if none there
+print(count)  # not working with other code above this code starting on 27-31
