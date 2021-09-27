29ba808430ea832c5fcc0b23272e536999062d1f
Cheryl Rossi Huy <crossihuy@ivytech.edu>
2021-09-26 22:27:29 -0400

    new code

--- better_calculator.py
+++ better_calculator.py
@@ -1,30 +1,38 @@
 def main():
-    function = ""
-    num1 = 0
-    num2 = 0
     q = "y"
-    function = input("""Do you want to +(add), -(subtract), *(multiply), or /(divide) \n""")
+while True:
+    try:
+        print("Please enter five numbers. ")
+        num_1 = float(input("What is your first number? "))
 
-    num_1 = float(input("What is your first number? "))
+        f1 = input("""Do you want to +(add), -(subtract), *(multiply), or /(divide) \n""")
+    
+        num_2 = float(input("What is your second number?\n "))
+        num_3 = float(input("What is your third number?\n "))
+        num_4 = float(input("What is your fourth number?\n "))
+        num_5 = float(input("What is your fith number?\n "))
+    except ValueError:
+        print("You did not enter a number. ")
+        continue	
 
-    num_2 = float(input("What is your second number? "))
-
-    if function == "+":
-        print("{} + {} = ".format(num_1, num_2))
-        print(num_1 + num_2)
-
-    elif function == "-":
-        print("{} - {} = ".format(num_1, num_2))
-        print(num_1 - num_2)
-
-    elif function == "*":
-        print("{} * {} = ".format(num_1, num_2))
-        print(num_1 * num_2)
-
-    elif function == "/":
-        print("{} / {} = ".format(num_1, num_2))
-        print(num_1 / num_2)
-        q = input("do you want to continue? y/n\n ").lower
-
-if __name__ == "__main__":
-    main()
+    if f1 == "+":
+        print("{} + {} + {} + {} + {} = ".format(num_1, num_2, num_3, num_4, num_5 ))
+        print(num_1 + num_2 + num_3 + num_4 + num_5 )
+    elif f1 == "-":   
+        print("{} - {} - {} - {} - {} = ".format(num_1, num_2, num_3, num_4, num_5))
+        print(num_1 + num_2 + num_3 + num_4 + num_5 )
+    elif f1 == "*":	
+        print("{} * {} * {} * {} * {} = ".format(num_1, num_2, num_3, num_4, num_5))
+        print(num_1 + num_2 + num_3 + num_4 + num_5 )
+    elif f1 == "/":
+        print("{} / {} / {} / {} / {} = ".format(num_1, num_2, num_3, num_4, num_5))
+        print(num_1 + num_2 + num_3 + num_4 + num_5 )
+    else:
+        print("Enter a valid function, ")
+        break
+while True:	
+    q = input("do you want to continue? y/n\n ").lower
+    if q == "y" or q == "" or q == "n":
+        continue
+else:
+    print("you did not make a selection. ")
